cde46242a5b9a79234fcd18643145999b0cbb954
Divyanshu Yadav <divycic1@gmail.com>
2025-02-02 20:57:16 +0530

    build logout

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,6 +10,7 @@ const Login = () => {
   const [password, setPassword] = useState("pass");
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [error, setError] = useState("");
 
   const handleLogin = async () => {
     try {
@@ -23,7 +24,7 @@ const Login = () => {
       dispatch(addUser(res.data.user));
       return navigate("/");
     } catch (err) {
-      console.error(err);
+      setError(err?.response?.data || "Something went wrong!");
     }
   };
 
@@ -56,6 +57,7 @@ const Login = () => {
               />
             </label>
           </div>
+          <p className="text-red-500">{error}</p>
           <div className="card-actions justify-center m-2">
             <button className="btn btn-primary" onClick={handleLogin}>
               Login
